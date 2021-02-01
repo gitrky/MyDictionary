cfafd3e115312cc9c86e13e1f374fd1f412bce69
Rukiye İsen <16.isen.rukiye@gmail.com>
2021-02-01 15:36:28 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<Tk, Tv>
+    {
+        Tk[]  Key;
+        Tv[] Value;
+        
+
+        public MyDictionary()
+        {
+            Key = new Tk[0];
+            Value = new Tv[0];
+        }
+        public void Add(Tk Tkey,Tv Tvalue)
+        {
+            
+            Tk[] temparraykey = Key;
+            Key = new Tk[temparraykey.Length + 1];
+            Tv[] temparrayvalue = Value;
+            Value = new Tv[temparrayvalue.Length + 1];
+
+            for (int i = 0; i <temparraykey.Length; i++)
+            {
+                Key[i] = temparraykey[i];
+               Value[i] = temparrayvalue[i];
+            }
+
+            Key[Key.Length - 1] = Tkey;
+            Value[Value.Length - 1] = Tvalue;
+
+            Console.WriteLine("Plaka: " + Tkey + "\t" + "İsim: " + Tvalue + "  ---->  sisteme eklenmiştir");
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> country = new MyDictionary<int, string>();
+            country.Add(16,"Bursa");
+            country.Add(10, "Balıkesir");
+            country.Add(34, "İstanbul");
+        }
+     
+     
+    }
+}
